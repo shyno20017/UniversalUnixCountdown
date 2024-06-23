c74da478b7cf64e0af9019b159f9142e1ba1b0b3
shyno20017 <34481827+shyno20017@users.noreply.github.com>
2024-06-23 10:27:51 +0000

    Version 0.0.1

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
 }
 
 function draw() {
-  noStroke()
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+  background(255);
+  fill(0, 0, 0);
+  text(Math.floor(Date.now() / 1000), 100, 100)
 }
